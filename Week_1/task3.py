a0727b909e6a1a322ef315acdcac171de5a5cbc9
CaraTortu <javierdj@jadk.com>
2023-09-18 18:31:59 +0100

    Add week 1 Work

---
+++ Week_1/task3.py
@@ -0,0 +1,6 @@
+print(2 > 7)           # False
+print(2 < 7)           # True
+print(3 == 3)          # True
+print(3 != 3)          # False
+print(3 > 2 and 4 < 3) # False
+print(3 > 2 or 4 < 3)  # True
